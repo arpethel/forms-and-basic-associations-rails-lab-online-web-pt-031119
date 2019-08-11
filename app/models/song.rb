34154e50577068161798a6d3ef63b953341117fe
Ryan Pethel <arpethel@gmail.com>
2019-08-11 00:07:45 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,6 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
 
-  def artist_name
-    self.artist.name
-  end
-
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
@@ -20,5 +16,13 @@ class Song < ActiveRecord::Base
 
   def genre_name
      self.genre ? self.genre.name : nil
+  end
+
+  def song_note=(content)
+    self.note = Note.find_or_create_by(content: content)
+  end
+
+  def song_note
+     self.note ? self.note.content : nil
   end
 end
