b19e2cc75577b2cc219d608cb15a1eb606dbd1f5
Ryan Pethel <arpethel@gmail.com>
2019-08-11 00:38:48 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -20,10 +20,14 @@ class Song < ActiveRecord::Base
   end
 
   def note_contents=(notes)
-    self.notes = Note.find_or_create_by(content: content)
+    notes.each do |content|
+      if content.strip != ''
+        self.notes.build(content: content)
+      end
+    end
   end
 
   def note_contents
-     self.notes ? self.notes.content : nil
+    self.notes.map(&:content)
   end
 end
